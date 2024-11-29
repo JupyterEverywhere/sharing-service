8ab664f8e286870ccd032d66bf3b353a6caf995e
billy-rojas <billy.rojas@edify.cr>
2024-11-29 10:53:51 -0600

    Update V2__add_password_to_jupyter_notebook.sql

--- src/main/resources/db/migration/V2__add_password_to_jupyter_notebook.sql
+++ src/main/resources/db/migration/V2__add_password_to_jupyter_notebook.sql
@@ -1,3 +1,3 @@
 -- Add a new column 'password' to the 'jupyter_notebook_metadata' table
 ALTER TABLE jupyter_notebooks_metadata
-    ADD COLUMN password VARCHAR(255);
+    ADD COLUMN IF NOT EXISTS password VARCHAR(255);
