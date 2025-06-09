353db9a6685a1cad6e163516d46f6bfc41b5e24d
Adam Blake <adam@coursekata.org>
2025-06-09 15:15:05 -0700

    feat: dynamically retrieve the app information from build properties

--- src/main/java/org/jupytereverywhere/controller/HealthController.java
+++ src/main/java/org/jupytereverywhere/controller/HealthController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.log4j.Log4j2;
 
+import org.jupytereverywhere.service.ApplicationInfoService;
+
 
 /**
  * Health check controller for monitoring the API status.
@@ -20,6 +22,12 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/health")
 public class HealthController {
 
+    private final ApplicationInfoService applicationInfoService;
+
+    public HealthController(ApplicationInfoService applicationInfoService) {
+        this.applicationInfoService = applicationInfoService;
+    }
+
     @GetMapping
     public ResponseEntity<Map<String, Object>> health() {
         log.info("Health check endpoint accessed");
@@ -28,8 +36,8 @@ public class HealthController {
         response.put("status", "OK");
         response.put("message", "Sharing Service API is running");
         response.put("timestamp", Instant.now().toString());
-        response.put("service", "sharing-service");
-        response.put("version", "0.1.1");
+        response.put("service", applicationInfoService.getName());
+        response.put("version", applicationInfoService.getVersion());
         
         return ResponseEntity.ok(response);
     }
